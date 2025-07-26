84713af173d10510f2d459c776706e19522ce867
Binit Semwal <binitsemwal@192.168.1.7>
2025-07-26 17:14:25 +0530

    update

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="KPA Form API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
